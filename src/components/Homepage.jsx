8a9410048ea8772d8184dedb9974bd2aa319ec90
sanjeev chauhan <sanjeevchhn76@gmail.com>
2021-09-19 15:43:14 +0530

    modified home component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -30,7 +30,7 @@ const Homepage = () => {
       total24hVolume: globalStats?.totalExchanges ? millify(globalStats?.total24hVolume) : 0,
       isFetching,
     });
-  }, []);
+  }, [data]);
 
   if (isFetching) return <Loader />;
 
@@ -40,27 +40,27 @@ const Homepage = () => {
         Global Crypto Stats
       </Title>
       <Row gutter={[32, 32]}>
-        <Col span={8}>
+        <Col xm={12} sm={12} md={8}>
           <Card hoverable>
             <Statistic valueStyle={{ color: '#1890ff' }} title="Total Cryptocurrencies" value={statsData.total} />
           </Card>
         </Col>
-        <Col span={8}>
+        <Col xm={12} sm={12} md={8}>
           <Card hoverable>
             <Statistic title="Total Exchanges" valueStyle={{ color: '#1890ff' }} value={statsData.totalExchanges} />
           </Card>
         </Col>
-        <Col span={8}>
+        <Col xm={12} sm={12} md={8}>
           <Card hoverable>
             <Statistic title="Total Market Cap" valueStyle={{ color: '#1890ff' }} value={`$${statsData.totalMarketCap}`} />
           </Card>
         </Col>
-        <Col span={8}>
+        <Col xm={12} sm={12} md={6}>
           <Card hoverable>
             <Statistic title="Total 24h Volume" valueStyle={{ color: '#1890ff' }} value={`$${statsData.total24hVolume}`} />
           </Card>
         </Col>
-        <Col span={8}>
+        <Col xm={12} sm={12} md={6}>
           <Card hoverable>
             <Statistic title="Total Markets" valueStyle={{ color: '#1890ff' }} value={statsData.totalMarkets} />
           </Card>
